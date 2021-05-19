3e05da74eb97faa6263bd360bff8b110ba872205
laughingsquirrel <keith@laughingsquirrel.net>
2021-05-19 15:33:14 -0400

    Initial Migration

---
+++ build.rs
@@ -0,0 +1,25 @@
+use std::io::Result;
+
+#[cfg(feature = "services")]
+fn main() -> Result<()> {
+    tonic_build::configure()
+        .build_server(false)
+        .type_attribute(".", "#[derive(serde_derive::Serialize)]")
+        .compile(
+            &[
+                "src/blockchain_txn.proto",
+                "src/service/router.proto",
+                "src/service/gateway.proto",
+            ],
+            &["src/"],
+        )?;
+    Ok(())
+}
+
+#[cfg(not(feature = "services"))]
+fn main() -> Result<()> {
+    prost_build::Config::new()
+        .type_attribute(".", "#[derive(serde_derive::Serialize)]")
+        .compile_protos(&["src/blockchain_txn.proto"], &["src/"])?;
+    Ok(())
+}
